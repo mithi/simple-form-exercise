bc861b3700580a92e3f4077e621a328579d296a5
Mithi <mithi.sevilla@gmail.com>
2021-03-18 19:58:57 +0800

    chore: Replace preliminary boilerplate generated by CRA

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from '.';
 
-test('renders learn react link', () => {
+test('Renders hello world app', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/hello world/i);
   expect(linkElement).toBeInTheDocument();
 });
