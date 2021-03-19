ec3c0cff2010183f9a546ff5678f209e54b86767
Mithi <mithi.sevilla@gmail.com>
2021-03-19 21:21:50 +0800

    feat: Add actual modal

--- src/App/index.js
+++ src/App/index.js
@@ -2,44 +2,39 @@ import "@reach/dialog/styles.css"
 import { useState } from "react"
 import { Dialog } from "@reach/dialog"
 import Layout from "./Layout"
+import RequestPassForm from "../components/RequestPassForm"
 
-function ModalExample() {
+function RequestPassModal() {
     const [showDialog, setShowDialog] = useState(false)
     const open = () => setShowDialog(true)
     const close = () => setShowDialog(false)
     return (
-        <div>
+        <>
             <button onClick={open}>Request a pass</button>
             <Dialog
-                aria-label="Thank you! Please keep an eye for your first class ticket on your email "
+                aria-label="Request a Pass Modal"
                 isOpen={showDialog}
                 onDismiss={close}
-                style={{ textAlign: "center" }}
             >
-                <h1>Thank you!</h1>
-                <p>
-                    Please keep an eye for your first class ticket on your email
-                </p>
-                <button onClick={close}>ok</button>
+                <RequestPassForm close={close} />
             </Dialog>
-        </div>
+        </>
     )
 }
 
 const App = () => {
     const footerContent = (
         <>
-            <span> Start your day with {"☕"}</span>
-            <br />
-            <span> ABC Group. All rights reserved.</span>
+            <div> Start your day with {"☕"}</div>
+            <div> ABC Group. All rights reserved.</div>
         </>
     )
 
     const mainContent = (
-        <div style={{ width: "500px", textAlign: "center" }}>
+        <div style={{ width: "500px" }}>
             <h1>Lorem Ipsum dolor sit amet consectetur adipiscing</h1>
             <div>Get your front row sneak peak with us!</div>
-            <ModalExample />
+            <RequestPassModal />
         </div>
     )
 
