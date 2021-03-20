46620cbbf2c7bfd87890278461aa29ffdc60a17e
Mithi <mithi.sevilla@gmail.com>
2021-03-20 21:29:05 +0800

    test: Clicking outside should remove the modal

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -6,15 +6,10 @@ import {
 } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
 import App from "../App"
-//import { willRandomlyReject, sleep } from "../hooks/utils"
-//jest.mock("../hooks/utils")
+//import { isRandomlyRejected, sleep } from "../utils"
+//jest.mock("../utils")
+
 /*
-
-TEST: Entering the modal
-
-click request a pass
-
-
 TEST: exiting the modal
 click request a pass
 clicking outside the modal (document body)
@@ -22,7 +17,7 @@ will exit the modal (we wont see the elements in (1))
 
 
 TEST
-- clicking the submit button with no correct field values will trigger the "requesting.." text on the button and be disabled
+- clicking the submit button with all correct field values will trigger the "requesting.." text on the button and be disabled
 - When success the modal will change to the exit popup view (which has an OK button etc), clicking the OK button will close the pop up
 - When error, the modal will show the same components (button, input fields, header) and show text that there is a problem with the server
 
@@ -87,9 +82,36 @@ describe("App", () => {
     test("Clicking the button will open the modal with the expected html elements", () => {
         render(<App />)
 
+        expect(
+            screen.queryByRole("dialog", { name: "Request a Pass Modal" })
+        ).not.toBeInTheDocument()
+
         const button = screen.getByRole("button", { name: /request a pass/i })
         userEvent.click(button)
 
+        expect(
+            screen.getByRole("dialog", { name: "Request a Pass Modal" })
+        ).toBeInTheDocument()
+
         modalExpect()
+    })
+
+    test("Clicking outside the modal should close the modal", async () => {
+        render(<App />)
+        const button = screen.getByRole("button", { name: /request a pass/i })
+        userEvent.click(button)
+
+        expect(
+            screen.getByRole("dialog", { name: "Request a Pass Modal" })
+        ).toBeInTheDocument()
+
+        userEvent.click(
+            screen.getByRole("dialog", { name: "Request a Pass Modal" })
+                .parentElement
+        )
+
+        expect(
+            screen.queryByRole("dialog", { name: "Request a Pass Modal" })
+        ).not.toBeInTheDocument()
     })
 })
